a7abe0f3358174624c6583075101c1fee80e07a2
Georgia Antoniou <63106778+georgiantoniou@users.noreply.github.com>
2023-06-15 13:01:18 +0100

    Update load_generator_open_loop.cc

--- microsuite/load_generator_open_loop.cc
+++ microsuite/load_generator_open_loop.cc
@@ -188,6 +188,9 @@ class RouterServiceClient {
                     char* cmd = new char[s.length() + 1];
                     std::strcpy(cmd, s.c_str());
                     ExecuteShellCommand(cmd);
+                    std::cout << "Call Status Error Code: " << call->status.error_code() << "\n";
+                    std::cout << "Call Status Error Message: " << call->status.error_message() << "\n";
+                    std::cout << "Call Status Error Details: " << call->status.error_details() << "\n";
                     std::cout << "Load generator failed\n";
                     CHECK(false, "");
                 }
@@ -395,6 +398,10 @@ class RouterServiceClient {
                
                 if (responses_recvd->AtomicallyReadCount() % 50000 == 0 && responses_recvd->AtomicallyReadCount() != 0){
                   std::cout << "Responses: " << responses_recvd->AtomicallyReadCount() << "\n";
+                }
+               
+               if (num_requests->AtomicallyReadCount() % 10000 == 0 && num_requests->AtomicallyReadCount() != 0){
+                  std::cout << "Requests: " << num_requests->AtomicallyReadCount() << "\n" ;
                 }
                 curr_time = (double)GetTimeInMicro();
             }
